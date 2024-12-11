aec91ebeb8463b5c00150010d4daa65473f2a75c
non_hana <1209220829@qq.com>
2024-12-11 11:56:30 +0800

    feat: 完善 about 页动画效果

--- components/about/Intro.vue
+++ components/about/Intro.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-const { data: about } = await useAsyncData('article-about', () => queryContent('/about').findOne())
-</script>
-
 <template>
-  <ContentRenderer v-if="about" :value="about" class="flex flex-col gap-4 leading-7" />
+  <ContentDoc path="/about" class="flex flex-col gap-4 leading-7" />
 </template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="flex flex-col">
-    <div class="relative h-[calc(100vh-80px)]">
-      <AboutBanner class="relative top-1/2 -translate-y-1/2 lg:top-1/4 lg:translate-y-0" />
-      <Icon class="absolute bottom-0 left-1/2 -translate-x-1/2 animate-bounce text-text" size="24" name="lucide:chevron-down" />
+    <div class="relative h-[calc(100vh-80px)] w-full">
+      <AboutBanner class="relative top-8 lg:top-1/4 lg:translate-y-0" />
+      <div class="absolute bottom-0 left-1/2 -translate-x-1/2 text-text">
+        <Icon class="animate-bounce" size="24" name="lucide:chevron-down" />
+      </div>
     </div>
     <div class="relative h-[calc(100vh-48px)]">
       <AboutSlogan class="relative top-1/2 -translate-y-1/2" />
-      <Icon class="absolute bottom-0 left-1/2 -translate-x-1/2 animate-bounce text-text" size="24" name="lucide:chevron-down" />
+      <div class="absolute bottom-0 left-1/2 -translate-x-1/2 text-text">
+        <Icon class="animate-bounce" size="24" name="lucide:chevron-down" />
+      </div>
     </div>
     <div class="relative my-20">
       <AboutIntro />
