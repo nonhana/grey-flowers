cf161a91d91974c55acc0c53f13215237c3d88e1
non_hana <1209220829@qq.com>
2025-01-11 19:35:51 +0800

    fix: 更新页脚版权年份

--- components/main/Footer.vue
+++ components/main/Footer.vue
@@ -51,7 +51,7 @@ onMounted(() => {
       <span>苗圃已浇灌 {{ runDays }} 天</span>
     </div>
     <div class="flex w-full flex-col gap-2 text-sm dark:text-hana-white md:flex-row md:items-center">
-      <span>Copyright © 2024 Hana's Garden</span>
+      <span>Copyright © 2024-2025 Hana's Garden</span>
       <span class="hidden text-text dark:text-hana-white-700 md:block">|</span>
       <span>Powered by <ProseA href="https://github.com/nonhana/GreyFlowers">GreyFlowers</ProseA></span>
       <span class="hidden text-text dark:text-hana-white-700 md:block">|</span>
