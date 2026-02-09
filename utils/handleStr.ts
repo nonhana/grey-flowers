5c03d1d133478cc2939f2abfe9718a54b310bed1
non_hana <nonhana@outlook.com>
2026-02-09 20:44:55 +0800

    fix: 添加 flatStr 方法

--- utils/handleStr.ts
+++ utils/handleStr.ts
@@ -1,3 +1,7 @@
+export function flatStr(str: string): string {
+  return str.trim().replace(/\s+/g, '-').toLowerCase()
+}
+
 export function antiFlatStr(str: string): string {
   const words = str.split('-')
   return words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
